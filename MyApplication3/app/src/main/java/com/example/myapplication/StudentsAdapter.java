17c2bb5009bb10d467c70b5ebd81350598431bf2
Mohammad-AlBalkhi <balkhimy08@gmail.com>
2022-08-24 14:25:05 +0300

    Done

---
+++ MyApplication3/app/src/main/java/com/example/myapplication/StudentsAdapter.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class StudentsAdapter extends ArrayAdapter {
+    List <Student> studentsList;
+    public StudentsAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+        super(context, resource, objects);
+        studentsList = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View View = LayoutInflater.from(getContext()).inflate(R.layout.xml2, parent , false);
+
+        Student currentstudent = studentsList.get(position);
+
+        TextView studentName = View.findViewById(R.id.SName);
+        ImageView studentImg = View.findViewById(R.id.SImg);
+        TextView studentAge = View.findViewById(R.id.SAge);
+        TextView studentGrade = View.findViewById(R.id.SGrade);
+
+        studentName.setText(currentstudent.getStudentName());
+        studentImg.setImageResource(currentstudent.getStudentImg());
+        studentAge.setText(currentstudent.getStudentAge());
+        studentGrade.setText(currentstudent.getStudentGrade());
+        return View;
+    }
+}
